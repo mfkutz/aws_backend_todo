d9222a7ae035d555863339860aa873f33bd35efa
Martin <mfkutz@gmail.com>
2024-11-15 12:24:57 -0300

    adding general url

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db.js');
+const connectDB = require("./config/db.js");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 //ROUTE IMPORTS
-const todoRoutes = require('./routes/todo');
+const todoRoutes = require("./routes/todo");
 
 //APP SETTINGS
 app.use(express.json());
 const PORT = process.env.PORT || 5050;
 app.use(cors());
 
-
 app.use("/healthcheck", (req, res) => {
-    res.status(200).send("ok");
-  });
+  res.status(200).send("ok");
+});
+app.use((req, res) => {
+  res.status(200).send("URL no encontrada");
+});
 app.use(`/api/${process.env.API_V1}/todo`, todoRoutes);
 
 //APP initialization
 connectDB();
-mongoose.connection.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
 //APP connection error
 mongoose.connection.on("error", (error) => {
-    console.log("Error connecting to db: ", error);
-  });
+  console.log("Error connecting to db: ", error);
+});
